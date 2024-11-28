084b9f1a74dd518f247bed6435a3232f900bdbdc
hudawattuqa <147321479+hudawattuqa@users.noreply.github.com>
2024-11-28 14:48:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+/*const sebuahNode = document.getElementById("h1")
+sebuahNode.innerHTML = "<em>Javascript</em>"*/
+
+/*
+const header1 = document.getElementById("h1") //elemen node
+const p = document.getElementsByTagName("p") // html collection
+const list = document.getElementsByClassName("list") // html collection
+
+header1.style.backgroundColor ="Purple"
+p[0].style.color = "Yellow"
+list[0].style.color = "Blue" */
+
+/*const list2 = document.querySelector("li:nth-child(2)")
+const anchor = document.querySelector("#a a")
+list2.style.backgroundColor = "Grey";
+anchor.style.fontsize = "40px";*/
+
+
+/*
+const nodeSection = document.getElementById("a")
+const nodeP = nodeSection.querySelector("p")
+const list = "<ol><li>ayam goreng</li><li>ayam rebus</li></ol>"
+nodeP.innerHTML = list
+*/
+
+/*const nodeH1 = document.getElementById("h1")
+nodeH1.style.fontFamily = "Consolas"
+nodeH1.style.backgroundColor = "Gold"
+nodeH1.style.textShadow = "10px 10px 10px"*/
+
+/*
+// Ganti warna background div
+document.querySelectorAll('#container1, #container2').forEach(div => {
+    div.style.backgroundColor = 'green'; // Ganti ke warna ke hijau
+});
+
+// Ganti isi atribut href di tag <a>
+const link = document.querySelector('#a a');
+link.href = 'https://www.google.com';
+
+// Loop ngubah text, style, size, & warna background
+const listItems = document.querySelectorAll('#container2 ul li');
+listItems.forEach((item, index) => {
+    // Ubah text
+    item.textContent = `List Baru ${index + 1}`;
+    // Ubah style
+    item.style.fontSize = '18px';
+    item.style.fontFamily = 'Arial, sans-serif';
+    // Ubah warna background
+    item.style.backgroundColor = 'gray';
+    item.style.color = 'white';
+});
+*/
